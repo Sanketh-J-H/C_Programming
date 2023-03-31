df4c4974a5414fa4d04b3da65dffc42a4da596c7
Sanketh-J-H <116152608+Sanketh-J-H@users.noreply.github.com>
2023-03-31 10:07:15 +0530

    new file:   08_strings/02_str_togg/makefile 	renamed:    08_strings/02_str_togg/02_str_togg.c -> 08_strings/02_str_togg/str_togg.c 	renamed:    08_strings/02_str_togg/02_str_togg.txt -> 08_strings/02_str_togg/str_togg.txt 	renamed:    08_strings/03_str_coun.c -> 08_strings/03_str_coun/03_str_coun.c 	renamed:    08_strings/03_str_coun.txt -> 08_strings/03_str_coun/03_str_coun.txt

--- 08_strings/02_str_togg/str_togg.c
+++ 08_strings/02_str_togg/str_togg.c
@@ -6,8 +6,8 @@
  * the proprietary works, trade secrets, and copyrights of Embitel or its licensors.
  * For further information, contact community.ruggedboard.com / info@ruggedboard.com
  *
- * Application: 
- * Brief: 
+ * Application:
+ * Brief:
  * Author: Sanketh J H
  * Title: Trainee
  * Last Modified Date: 31.03.2023
@@ -15,6 +15,31 @@
 /*********************************************************************************************/
 
 // Header File
+#include <stdio.h>
+#include <string.h>
 
+int main()
+{
+     char str[20];
+     int i;
+
+     printf("Enter a string: ");
+     scanf("%[^\n]s", str); // Use %[^\n]s instead of %s to read the entire line
+
+     for (i = 0; str[i]; i++)
+     {
+          if (str[i] >= 'A' && str[i] <= 'Z')
+          {
+               str[i] = str[i] + ('a' - 'A');
+          }
+          else if (str[i] >= 'a' && str[i] <= 'z')
+          {
+               str[i] = str[i] - ('a' - 'A');
+          }
+     }
+
+     printf("The toggle case is : %s\n", str);
+     return 0;
+}
 
 // Program End
