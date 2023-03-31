0c4ff5cd5bc3b53fe6587cf4f56d7bc02bb1d72d
Sanketh-J-H <sankethjh007@gmail.com>
2023-03-31 15:37:55 +0530

    deleted:    09_structures/03_str_add/str_add 	new file:   09_structures/04_str_tim/makefile 	new file:   09_structures/04_str_tim/str_tim.c 	renamed:    09_structures/04_str_tim.txt -> 09_structures/04_str_tim/str_tim.txt 	deleted:    09_structures/05_str_info.c 	new file:   09_structures/05_str_info/makefile 	renamed:    09_structures/04_str_tim.c -> 09_structures/05_str_info/str_info.c 	renamed:    09_structures/05_str_info.txt -> 09_structures/05_str_info/str_info.txt

--- 09_structures/05_str_info/str_info.c
+++ 09_structures/05_str_info/str_info.c
@@ -6,21 +6,45 @@
  * the proprietary works, trade secrets, and copyrights of Embitel or its licensors.
  * For further information, contact community.ruggedboard.com / info@ruggedboard.com
  *
- * Application: 
- * Brief: 
- * Author: Natarajan S  ( natarajan.s@ruggedboard.com )
- * Title: Program Manager / Trainner
- * Last Modified Date: 19.09.2022
+ * Application:
+ * Brief:
+ * Author: Sanketh J H
+ * Title: Trainee
+ * Last Modified Date: 31.03.2023
  *
 /*********************************************************************************************/
 
 // Header File
 #include <stdio.h>
 
-// Main Function
+struct student
+{
+     char name[50];
+     float marks;
+} s[5];
+
 int main()
 {
-     
+     int i;
+
+     printf("Enter information of students:\n");
+
+     // storing information
+     for (i = 0; i < 5; ++i)
+     {
+          printf("\nFor roll number%d,\nEnter first name: ", i + 1);
+          scanf("%s", s[i].name);
+
+          printf("Enter marks: ");
+          scanf("%f", &s[i].marks);
+     }
+
+     printf("\nDisplaying Information:\n\n");
+
+     // displaying information
+     for (i = 0; i < 5; ++i)
+          printf("Roll number:%d\nFirst name: %s\nMarks: %.1f\n\n", i + 1, s[i].name, s[i].marks);
+
      return 0;
 }
 
