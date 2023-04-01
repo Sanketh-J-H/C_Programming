fb4fd44987d4025afb7aaa618c74d08d085564d2
Sanketh-J-H <sankethjh007@gmail.com>
2023-04-01 17:45:51 +0530

    renamed:    10_files/05_fil_num/05_fil_num.c -> 10_files/05_fil_num/fil_num.c 	renamed:    10_files/05_fil_num/05_fil_num.txt -> 10_files/05_fil_num/fil_num.txt 	new file:   10_files/05_fil_num/makefile 	new file:   10_files/05_fil_num/test.txt

--- 10_files/05_fil_num/fil_num.c
+++ 10_files/05_fil_num/fil_num.c
@@ -6,8 +6,8 @@
  * the proprietary works, trade secrets, and copyrights of Embitel or its licensors.
  * For further information, contact community.ruggedboard.com / info@ruggedboard.com
  *
- * Application: 
- * Brief: 
+ * Application:
+ * Brief:
  * Author: Sanketh J H
  * Title: Trainee
  * Last Modified Date: 01.04.2023
@@ -15,6 +15,42 @@
 /*********************************************************************************************/
 
 // Header File
+#include <stdio.h>
+#include <stdlib.h>
 
+int main()
+{
+     char filename[100];
+     FILE *fp;
+     int count = 0;
+     char c;
+
+     printf("Read the file and count the number of lines :\n");
+     printf("--------------------------------------------------\n");
+
+     printf("Input the file name to be opened : ");
+     scanf("%s", filename);
+
+     fp = fopen(filename, "r");
+     if (fp == NULL)
+     {
+          printf("Failed to open file\n");
+          return 1;
+     }
+
+     while ((c = fgetc(fp)) != EOF)
+     {
+          if (c == '\n')
+          {
+               count++;
+          }
+     }
+
+     fclose(fp);
+
+     printf("The lines in the file %s are : %d\n", filename, count);
+
+     return 0;
+}
 
 // Program End
